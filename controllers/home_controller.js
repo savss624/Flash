77c5373e328a758f61445238de933c09e0dc6e12
savss_624 <parth0803@gmail.com>
2021-02-08 00:28:10 +0530

    setup assets, layouts and Partials

--- controllers/home_controller.js
+++ controllers/home_controller.js
@@ -1,3 +1,5 @@
 module.exports.home = function(req, res) {
-    res.end('home page');
+    res.render('home', {
+        title: "FlashChat"
+    });
 }
--- index.js
+++ index.js
@@ -1,8 +1,17 @@
 const port = 8000;
 const express = require('express');
 const app = express();
+const expressLayout = require('express-ejs-layouts');
+
+app.use(express.static('./assets'));
+app.use(expressLayout);
+app.set('layout extractStyles', true);
+app.set('layout extractScripts', true);
 
 app.use('/', require('./routes'));
+
+app.set('view engine', 'ejs');
+app.set('views', './views');
 
 app.listen(port, function(err) {
     if(err) {
